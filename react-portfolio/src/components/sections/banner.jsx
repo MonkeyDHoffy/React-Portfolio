caf95e5dd45e997e9c43be9031cbcdfe48e246a3
MonkeyDHoffy <jonekstoff@googlemail.com>
2026-01-25 19:58:51 +0100

    bannernaimation beta

--- react-portfolio/src/components/sections/banner.jsx
+++ react-portfolio/src/components/sections/banner.jsx
@@ -5,15 +5,25 @@ import { useLang } from '../../context/LanguageContext';
 export default function Banner({ children }) {
   const { t } = useLang();
 
-  return (
-    <section className="banner-container gap-56">
-      {/* direkte Kinder für gap */}
+  const content = (
+    <>
       <div className="text-text-secondary font-karla text-[32px] leading-[120%]">{t('hero.bannerone')}</div>
       <div style={{ width: '8px', height: '8px', backgroundColor: '#3DCFB6', borderRadius: '50%' }}></div>
       <div className="text-text-secondary font-karla text-[32px] leading-[120%]">{t('hero.bannertwo')}</div>
       <div style={{ width: '8px', height: '8px', backgroundColor: '#3DCFB6', borderRadius: '50%' }}></div>
       <div className="text-text-secondary font-karla text-[32px] leading-[120%]">{t('hero.bannerthree')}</div>
+      <div style={{ width: '8px', height: '8px', backgroundColor: '#3DCFB6', borderRadius: '50%' , marginRight: '56px'}}></div>
+    </>
+  );
+
+  return (
+    <div className="banner-wrapper">
+      <div className="banner-track">
+        <div className="banner-content">{content}</div>
+        <div className="banner-content">{content}</div>
+        <div className="banner-content">{content}</div>
+      </div>
       {children}
-    </section>
+    </div>
   );
 }
